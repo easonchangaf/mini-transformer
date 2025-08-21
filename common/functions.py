bd714c65dd98fb4f8bd1f64b2977a6c52e815f89
easonchangaf <easonchangaf@gmail.com>
2025-08-21 11:03:37 +0800

    update

--- common/functions.py
+++ common/functions.py
@@ -67,22 +67,6 @@ def numerical_diff(f: Callable, x: float) -> float:
     return (f(x+h) - f(x-h)) / (2*h)
 
 
-def numerical_gradient(f: Callable, x: "numpy.ndarray"):
-    h = 1e-4 # 0.0001
-    grad = np.zeros_like(x) # 生成和x形状相同的数组
-    for idx in range(x.size):
-        tmp_val = x[idx]
-        # f(x+h)的计算
-        x[idx] = tmp_val + h
-        fxh1 = f(x)
-        # f(x-h)的计算
-        x[idx] = tmp_val - h
-        fxh2 = f(x)
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        x[idx] = tmp_val # 还原值
-    return grad
-
-
 def squard(x:float) -> float:
     return x ** 2
 
---
+++ common/gradient.py
@@ -0,0 +1,18 @@
+from typing import Callable
+import numpy as np
+
+
+def numerical_gradient(f: Callable, x: "numpy.ndarray"):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x) # 生成和x形状相同的数组
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h)的计算
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+        # f(x-h)的计算
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val # 还原值
+    return grad
